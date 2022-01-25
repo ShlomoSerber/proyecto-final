83bcdaf4da0ff6b7b594b94351be15b521f13bcd
Aaron <feldmanaaron406@gmail.com>
2022-01-24 21:07:53 -0300

    cambioenHome

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,7 +4,6 @@ const Home = () => {
     return ( 
         <>
             <h1>Este es el Home</h1>
-            <button onClick={() => setearTest('chau')}>setearElTest</button>
         </>
      );
 }
