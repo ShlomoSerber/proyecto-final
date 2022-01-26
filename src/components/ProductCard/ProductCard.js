0a012015a7a059a65da8f3e8d4eebe0536b2db81
ShlomoSerber <ss.negf@gmail.com>
2022-01-26 12:56:58 -0300

    correccion al ProductCard

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -8,19 +8,17 @@ function ProductCard({productos}) {
                 {
                 productos.length > 0 ?
                 productos.map(producto =>
-                    <Link className="no-style" to={`/productos/producto/${producto.id}`} key={producto.id}>
-                        <div className="producto-card">
-                            <img src={producto.urlImagen} className="producto-imagen" alt=""/>
-                            <div className="producto-text">
-                                <h5 className="producto-title">{producto.nombre}</h5>
-                                <p className="producto-description">{producto.descripcionChica}</p>
-                                <p className="producto-description">{`Precio: $${producto.precio}`}</p>
-                                {
-                                    producto.stock === 0 ? 
-                                    <p className="producto-no-stock">No hay Stock</p>
-                                    : <div></div>
-                                }
-                            </div>
+                    <Link className="producto-card" to={`/productos/producto/${producto.id}`} key={producto.id}>
+                        <img src={producto.urlImagen} className="producto-imagen" alt=""/>
+                        <div className="producto-text">
+                            <h5 className="producto-title">{producto.nombre}</h5>
+                            <p className="producto-description">{producto.descripcionChica}</p>
+                            <p className="producto-description">{`Precio: $${producto.precio}`}</p>
+                            {
+                                producto.stock === 0 ? 
+                                <p className="producto-no-stock">No hay Stock</p>
+                                : <div></div>
+                            }
                         </div>
                     </Link>
                 )
